beab10a49065ac23306b89a2178710bb60125424
jiyeon2411 <daz128@naver.com>
2023-05-11 18:16:57 +0900

    수정

--- src/Seat/FirstClass.java
+++ src/Seat/FirstClass.java
@@ -4,7 +4,14 @@ public class FirstClass implements SeatClass {
 
 	@Override
 	public String getClass(int fare) {
-		return null;
+		if(fare <= 500) {
+			return "Economy";
+		} else if(fare >= 100 && fare <= 500) {
+			return "Business";
+		} else {
+			return "First";
+		}
+		
 	}
 
 }
